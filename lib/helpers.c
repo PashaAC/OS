dde8a8704f264e62e0ac22233dbf29ecb1fcbad4
Pavel Asadchiy <pashaac26@gmail.com>
2015-03-17 15:38:46 +0300

    read_until added

--- lib/helpers.c
+++ lib/helpers.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 #include "helpers.h"
 
-ssize_t read_(int fd, void * buffer, size_t count)
+ssize_t read_(int fd, void * buf, size_t count)
 {
     size_t offset = 0;
     while (offset < count) 
     {
-        ssize_t read_block = read(fd, buffer + offset, count - offset);
+        ssize_t read_block = read(fd, buf + offset, count - offset);
         if (read_block < 0) 
         {
             return read_block;
@@ -19,13 +19,13 @@ ssize_t read_(int fd, void * buffer, size_t count)
     }
     return offset;
 }
-       
-ssize_t write_(int fd, const void * buffer, size_t count) 
+
+ssize_t write_(int fd, const void * buf, size_t count) 
 {
     size_t offset = 0;
     while (offset < count) 
     {
-        ssize_t write_block = write(fd, buffer + offset, count - offset);
+        ssize_t write_block = write(fd, buf + offset, count - offset);
         if (write_block < 0) 
         {
             return write_block;
@@ -35,3 +35,28 @@ ssize_t write_(int fd, const void * buffer, size_t count)
     return offset;
 }
 
+ssize_t read_until(int fd, void * buf, size_t count, char delimiter) 
+{    
+    size_t offset = 0;
+    while (offset < count) 
+    {
+        ssize_t read_block = read(fd, buf + offset, count - offset);
+        if (read_block < 0) 
+        {
+            return read_block;
+        }
+        if (read_block == 0) 
+        {
+            break;
+        }
+        for (size_t i = offset; i != offset + read_block; ++i)
+        {
+            if (((char *)buf)[i] == delimiter)
+            {
+                return offset + read_block;
+            }
+        }
+        offset += read_block;
+    }
+    return offset;
+}
--- lib/helpers.h
+++ lib/helpers.h
@@ -1,7 +1,8 @@
 #ifndef HELPERS_H_
 #define HELPERS_H_
 
-ssize_t read_(int fd, void * buffer, size_t count);
-ssize_t write_(int fd, const void * buffer, size_t count);
+ssize_t read_(int fd, void * buf, size_t count);
+ssize_t write_(int fd, const void * buf, size_t count);
+ssize_t read_until(int fd, void * buf, size_t count, char delimiter);
 
 #endif
