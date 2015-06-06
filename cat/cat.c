6c5d2aa249af872587bf725f650fa4f69d891388
Pavel Asadchiy <pashaac26@gmail.com>
2015-06-06 11:44:11 +0300

    #1 cat.c changed

--- cat/cat.c
+++ cat/cat.c
@@ -1,28 +1,19 @@
 #include <unistd.h>
 #include <stdlib.h>
-#include "helpers.h"
+#include <helpers.h>
 
-#define BUFFER_SIZE 2048 
+#define BUFFER_SIZE 4096
 
-int main() 
-{
+int main() {
     char buffer[BUFFER_SIZE];
-    for (;;) 
-    {
+    while (1) {
         ssize_t read_block = read_(STDIN_FILENO, buffer, BUFFER_SIZE);
-        if (read_block < 0)
-        {
+        if (read_block == -1)
             exit(EXIT_FAILURE);
-        }
-        ssize_t write_block = write_(STDOUT_FILENO, buffer, read_block);
-        if (write_block < 0) 
-        {
-            exit(EXIT_FAILURE);
-        }
-        if (write_block == 0)
-        {
+        if (read_block == 0)
             break;
-        }
+        if (write_(STDOUT_FILENO, buffer, read_block) == -1)
+            exit(EXIT_FAILURE);
     }
     exit(EXIT_SUCCESS);
 }
