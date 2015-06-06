d26d63190b9e7537e843ce8558b19936f1ee99de
Pavel Asadchiy <pashaac26@gmail.com>
2015-06-06 23:10:49 +0300

    bufcat utility added

--- bufcat/bufcat.c
+++ bufcat/bufcat.c
@@ -1,30 +1,20 @@
-#include "bufio.h"
+#include <bufio.h>
 
-#define BUFFER_CAPACITY 2048
+#define BUFFER_SIZE 4096
 
-int main()
-{
-    struct buf_t * buf = buf_new(BUFFER_CAPACITY);
-    for (;;) 
-    {
-        size_t required = buf_capacity(buf) - buf_size(buf);
-        ssize_t curr_size = buf_fill(STDIN_FILENO, buf, required);
-        if (curr_size < required)
-        {
-            if (buf_flush(STDOUT_FILENO, buf, buf_size(buf)) == -1 || curr_size == -1)
-            {
-                buf_free(buf);
-                exit(EXIT_FAILURE);
-            }
-            buf_free(buf);
-            break;
-        }
+int main() {
+    struct buf_t * buf = buf_new(BUFFER_SIZE);
+    if (buf == NULL)
+        exit(EXIT_FAILURE);
+    ssize_t fill_block = 1;
+    do {
+        fill_block = buf_fill(STDIN_FILENO, buf, buf_capacity(buf) - buf_size(buf));
         if (buf_flush(STDOUT_FILENO, buf, buf_size(buf)) == -1)
-        {
-            buf_free(buf);
-            exit(EXIT_FAILURE);
-        }
-    }
+                exit(EXIT_FAILURE);
+    } while (fill_block > 0);
+    if (buf_flush(STDOUT_FILENO, buf, buf_size(buf)) == -1)
+        exit(EXIT_FAILURE);
+    buf_free(buf);
     exit(EXIT_SUCCESS);
 } 
 
